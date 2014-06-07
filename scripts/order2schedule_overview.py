672d39df9adf6bcc43acb30124233027720430bf
Matt Post <post@cs.jhu.edu>
2014-06-07 00:08:30 -0400

    Working

--- scripts/order2schedule_overview.py
+++ scripts/order2schedule_overview.py
@@ -26,15 +26,35 @@ args = PARSER.parse_args()
 if not os.path.exists(args.output_dir):
     os.makedirs(args.output_dir)
 
+def time_min(a, b):
+    ahour, amin = a.split(':')
+    bhour, bmin = b.split(':')
+    if ahour == bhour:
+        if amin < bmin:
+            return a
+        elif amin > bmin:
+            return b
+        else:
+            return a
+    elif ahour < bhour:
+        return a
+    else:
+        return b
+
+def time_max(a, b):
+    if time_min(a, b) == a:
+        return b
+    return a
+
 # List of dates
 dates = []
 schedule = defaultdict(defaultdict)
+sessions = defaultdict()
 
 for line in sys.stdin:
     line = line.rstrip()
 
     print "LINE", line
-
 
     if line.startswith('*'):
         day, date, year = line[2:].split(', ')
@@ -58,19 +78,22 @@ for line in sys.stdin:
             timerange = line.split(' ')[1]
             start, stop = timerange.split('--')
 
-            if session_start:
-                timerange = '%s--%s' % (session_start, session_stop)
-                schedule[(day, date, year)][timerange].pop(-1)
-                print "* DELETING", timerange, session_name
-            else:
-                session_start = start
+            if sessions.has_key(session_name):
+                old_start, old_stop = sessions[session_name][3].split('--')
+                if old_start != start or old_stop != stop:
+                    old_timerange = '%s--%s' % (old_start, old_stop)
+                    new_timerange = '%s--%s' % (time_min(old_start, start), time_max(old_stop, stop))
 
-            session_stop = stop
-            timerange = '%s--%s' % (session_start, session_stop)
-            if not schedule[(day, date, year)].has_key(timerange):
-                schedule[(day, date, year)][timerange] = []
-            schedule[(day, date, year)][timerange].append(session_name)
-            print "* CREATING", timerange, session_name
+                    sessions[session_name] = (day, date, year, new_timerange)
+
+            else:
+                sessions[session_name] = (day, date, year, timerange)
+
+for session in sorted(sessions.keys()):
+    day, date, year, timerange = sessions[session]
+    if not schedule[(day, date, year)].has_key(timerange):
+        schedule[(day, date, year)][timerange] = []
+    schedule[(day, date, year)][timerange].append(session)
 
 def sort_times(a, b):
     ahour, amin = a[0].split('--')[0].split(':')

dd29db6913cc1607f657c5fe68fe5572692b708d
Matt Post <post@cs.jhu.edu>
2015-05-05 12:55:50 -0400

    more escaping of latex-harmful chars

--- scripts/meta2bibtex.py
+++ scripts/meta2bibtex.py
@@ -15,6 +15,8 @@ from paper_info import *
 import sys, os, unicodedata, codecs
 import re
 
+sys.stdout = codecs.getwriter('utf-8')(sys.stdout)
+
 fdir = sys.argv[1] # i.e., $ACLPUB_ROOT/final
 tag  = sys.argv[2] # e.g., main, demos, ws1, ...
 
@@ -29,11 +31,21 @@ except:
     pass
 
 def escape(str):
-    str = str.replace('%','\%').replace('~','{\\textasciitilde}')
-    str = re.sub(r'([^$])\^(.*?) ', r'\1$^\2$ ', str)
+    """Replaces unescaped special characters with escaped versions, and does
+    other special character conversions."""
+    
+    str = str.replace('~','{\\textasciitilde}')
+
+    # escape these characters if not already escaped
+    special_chars = r'\#\@\&\$\_\%'
+    patternstr = r'([^\\])([%s])' % (special_chars)
+    str = re.sub(patternstr, '\\1\\\\\\2', str)
+
+    # fix superscripts
+#    str = re.sub(r'([^$])\^(.*?) ', r'\1$^\2$ ',  str)
     return str
 
-paper_ids = [int(n) for n in os.listdir(fdir)]
+paper_ids = [int(n) for n in filter(lambda x: re.match(r'^\d+$', x), os.listdir(fdir))]
 BIBFILE   = open("auto/"+tag+"/papers.bib",'w')
 for n in paper_ids:
     n = int(n)
@@ -41,12 +53,12 @@ for n in paper_ids:
     author = " and ".join(["%s, %s" % (a.last, a.first) for a in p.authors])
     sortname = ''.join([c for c in unicodedata.normalize('NFD', unicode(author))
                         if unicodedata.category(c) != 'Mn'])
-    #print author.encode("utf-8")
-    #print sortname.encode("utf-8")
+
+#    print "%s %s -> %s" % (p.id, p.long, escape(p.long))
     print >>BIBFILE, "@INPROCEEDINGS{%s-%03d," % (tag, int(p.id))
     print >>BIBFILE, "   AUTHOR = {%s}," % author.encode("utf-8")
     print >>BIBFILE, "   SORTNAME = {%s}," % sortname.encode("utf-8")
-    print >>BIBFILE, "   TITLE = {%s}}" % p.long.encode("utf-8")
+    print >>BIBFILE, "   TITLE = {%s}}" % escape(p.long).encode("utf-8")
     ABS = open("auto/abstracts/%s-%03d.tex" % (tag, n),'w')
     print >>ABS, escape(p.abstract).encode("utf-8")
     

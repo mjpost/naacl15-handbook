7ecf7314079559b8b37e3b587b8ad86c853cff67
Matt Post <post@cs.jhu.edu>
2015-05-06 16:08:43 -0400

    Added TACL yaml DB and generation script

---
+++ scripts/tacl_builder.py
@@ -0,0 +1,42 @@
+#!/usr/bin/env python 
+# -*- coding: utf-8 -*-
+
+"""
+Takes a YAML file with TACL paper information and generates the abstract and bib files
+needed to incorporate them in the handbook.
+
+auto/tacl/papers.bib
+audo/abstracts/tacl-XXX.tex
+"""
+
+import re, os
+import sys, csv
+import argparse
+import codecs
+from handbook import latex_escape
+import yaml
+
+PARSER = argparse.ArgumentParser(description="Generate schedules for *ACL handbooks")
+PARSER.add_argument("-yaml", default='input/tacl_papers.yaml', type=str, help="File path for TACL YAML file")
+args = PARSER.parse_args()
+
+for path in ['auto/tacl', 'auto/abstracts']:
+    if not os.path.exists(path):
+        os.makedirs(path)
+
+def bib(paper):
+    str  = '@INPROCEEDINGS{%s,\n' % (paper['id'])
+    str += '  AUTHOR = {%s},\n' % (paper['authors'])
+    str += '  TITLE = {%s},\n' % (latex_escape(paper['title']))
+    str += '  SORTNAME = {%s}}\n\n' % (paper['title'])
+    return str
+
+bibfile = codecs.open('auto/tacl/papers.bib', 'w', encoding = 'utf-8')
+for paper in yaml.load(open(args.yaml)):
+    bibfile.write(bib(paper))
+
+    abstract = codecs.open('auto/abstracts/%s.tex' % (paper['id']), 'w', encoding='utf-8')
+    abstract.write(latex_escape(paper['abstract']))
+    abstract.close()
+
+bibfile.close()

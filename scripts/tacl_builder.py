e934af01964432c565d79a1dc5c72bd599def3ce
Matt Post <post@cs.jhu.edu>
2015-05-08 11:53:54 -0400

    Fixing names and other typos

--- scripts/tacl_builder.py
+++ scripts/tacl_builder.py
@@ -31,10 +31,14 @@ def bib(paper):
     str += '  SORTNAME = {%s}}\n\n' % (paper['title'])
     return str
 
+print >> sys.stderr, "Reading from file", args.yaml
+print >> sys.stderr, "Write bibtex entries to auto/tacl/papers.bib"
+
 bibfile = codecs.open('auto/tacl/papers.bib', 'w', encoding = 'utf-8')
 for paper in yaml.load(open(args.yaml)):
     bibfile.write(bib(paper))
 
+    print >> sys.stderr, "Writing abstract auto/abstracts/%s.tex" % (paper['id'])
     abstract = codecs.open('auto/abstracts/%s.tex' % (paper['id']), 'w', encoding='utf-8')
     abstract.write(latex_escape(paper['abstract']))
     abstract.close()

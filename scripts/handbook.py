616e573df3dfa75264fea17c2e04094514381cff
Matt Post <post@cs.jhu.edu>
2015-05-09 21:59:49 -0400

    Moved classes

--- scripts/handbook.py
+++ scripts/handbook.py
@@ -29,3 +29,67 @@ def latex_escape(str):
     # fix superscripts
 #    str = re.sub(r'([^$])\^(.*?) ', r'\1$^\2$ ',  str)
     return str
+
+class Paper:
+    def __init__(self, line, subconf):
+        self.id, rest = line.split(' ', 1)
+        if re.match(r'^\d+', rest) is not None:
+            self.time, comment = rest.split(' ', 1)
+            self.poster = False
+        else:
+            self.time = None
+            self.poster = True
+            comment = rest
+
+        if self.id.find('/') != -1:
+            tokens = self.id.split('/')
+            self.id = '%s-%s' % (tokens[1].lower(), threedigits(tokens[0]))
+        else:
+            self.id = '%s-%s' % (subconf, threedigits(self.id))
+            
+    def __str__(self):
+        return "%s %s" % (id, time)
+
+class Session:
+    def __init__(self, line, date):
+        (self.time, namestr) = line[2:].split(' ', 1)
+        self.date = date
+        self.papers = []
+        self.desc = None
+
+        (self.name, self.keywords) = extract_keywords(namestr)
+        
+        if self.name.find(':') != -1:
+            colonpos = self.name.find(':')
+            self.desc = self.name[colonpos+2:]
+            self.name = self.name[:colonpos]
+            self.num = self.name.split(' ')[-1][:-1]
+        # print >> sys.stderr, "LINE %s NAME %s DESC %s" % (line, self.name, self.desc)
+
+        self.poster = False
+        l = self.name.lower()
+        if 'poster' in l or 'demo' in l or 'best paper' in l:
+            self.poster = True
+
+    def __str__(self):
+        return "SESSION [%s/%s] %s %s" % (self.date, self.time, self.name, self.desc)
+
+    def add_paper(self,paper):
+        self.papers.append(paper)
+
+    def chair(self):
+        """Returns the (first name, last name) of the chair, if found in a %chair keyword"""
+        
+        if self.keywords.has_key('chair'):
+            fullname = self.keywords['chair']
+            if ',' in fullname:
+                names = fullname.split(', ')
+                return (names[1].strip(), names[0].strip())
+            else:
+                # This is just a heuristic, assuming the first name is one word and the last
+                # name is 1+ words
+                names = fullname.split(' ', 1)
+                return (names[0].strip(), names[1].strip())
+        else:
+            return ('', '')
+
